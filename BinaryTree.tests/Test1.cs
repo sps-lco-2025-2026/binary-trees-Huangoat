16ebddc44bf84e5550b038135f45005af4a41c78
Jason Huang <huangjason@stpaulsschool.org.uk>
2026-03-16 11:55:59 +0000

    firxt check in

---
+++ BinaryTree.tests/Test1.cs
@@ -0,0 +1,17 @@
+﻿namespace BinaryTree.tests;
+using BinaryTree; 
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestDuplicates()
+    {
+        Tree tree = new Tree();
+        tree.Add(5);
+        tree.Add(3);
+        tree.Add(7);
+        tree.Add(7);
+        Assert.IsTrue(tree.IsDuplicates());
+    }
+}
